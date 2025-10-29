f8aa7081b2fbedcc51edd80cf5eb794c1bc511f3
przemys-aw-zawadka <zawadkaprzemek@gmail.com>
2025-10-29 13:15:33 +0100

    Fix: Fix PaymentProcessor

--- payments-svc/src/State/PaymentProcessor.php
+++ payments-svc/src/State/PaymentProcessor.php
@@ -2,25 +2,27 @@
 
 declare(strict_types=1);
 
-namespace App\DataPersister;
+namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Payment;
 use App\Service\PaymentEventPublisher;
 
-final readonly class PaymentDataPersister implements ProcessorInterface
+final class PaymentProcessor implements ProcessorInterface
 {
     public function __construct(
-        private ProcessorInterface    $decorated,
-        private PaymentEventPublisher $publisher
-    ) {}
+        private readonly ProcessorInterface    $decorated,
+        private readonly PaymentEventPublisher $publisher,
+    )
+    {
+    }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        $result = $this->decorated->process($data, $operation);
+        $result = $this->decorated->process($data, $operation, $uriVariables, $context);
 
-        if ($data instanceof Payment && ($context['collection_operation_name'] ?? null) === 'post') {
+        if ($data instanceof Payment && $operation->getMethod() === 'POST') {
             $this->publisher->publishCreated($data);
         }
 
